ec267cf0b20048cb35581454d53bb411fffda970
reneederer <rene.ederer@protonmail.com>
2025-05-22 23:50:56 +0200

    -

--- src/Client/DragDrop.fs
+++ src/Client/DragDrop.fs
@@ -6,22 +6,22 @@ open Feliz
 open Fable.React
 
 [<Import("useDraggable", from="@dnd-kit/core")>]
-let private useDraggableHook: obj -> obj = jsNative
+let private useDraggable: obj -> obj = jsNative
 
 [<Import("useDroppable", from="@dnd-kit/core")>]
-let private useDroppableHook: obj -> obj = jsNative
+let private useDroppable: obj -> obj = jsNative
 
 [<Import("DndContext", from="@dnd-kit/core")>]
-let private dndContextComponent: obj = jsNative
+let private dndContext: obj = jsNative
 
 [<Import("PointerSensor", from="@dnd-kit/core")>]
 let private pointerSensor: obj = jsNative
 
 [<Import("useSensor", from="@dnd-kit/core")>]
-let private useSensorHook: obj -> obj = jsNative
+let private useSensor: obj -> obj = jsNative
 
 [<Import("useSensors", from="@dnd-kit/core")>]
-let private useSensorsHook: obj -> obj = jsNative
+let private useSensors: obj -> obj = jsNative
 
 type DraggableProps = { 
     id: string 
@@ -33,46 +33,37 @@ type DroppableProps = {
     render: bool -> ReactElement 
 }
 
-[<Emit("Object.keys($0)")>]
-let private objectKeys (x: obj) : string[] = jsNative
-
 [<ReactComponent>]
 let Draggable (props: DraggableProps) =
-    let dnd = useDraggableHook {| id = props.id |}
+    let dnd = useDraggable {| id = props.id |}
     
-    // Create base props
-    let baseProps = [
+    let transformStyle = 
+        if not (isNull (box dnd?transform)) then
+            let x = unbox<float> dnd?transform?x
+            let y = unbox<float> dnd?transform?y
+            [ style.transform.translate(int x, int y) ]
+        else
+            []
+
+    Html.div [
         prop.ref (unbox dnd?setNodeRef)
-        prop.style [
+        prop.style ([
             style.cursor.grab
             style.border(2, borderStyle.solid, color.red)
             style.padding 10
             style.marginBottom 8
             style.backgroundColor "#fee2e2"
-        ]
+            style.position.relative
+            style.zIndex (if unbox<bool> dnd?isDragging then 1 else 0)
+        ] @ transformStyle)
+        prop.onPointerDown (unbox dnd?listeners?onPointerDown)
+        prop.onKeyDown (unbox dnd?listeners?onKeyDown)
         prop.children [ props.content ]
     ]
 
-    // Helper function to safely convert JS props
-    let convertJsProps (jsProps: obj) =
-        if isNullOrUndefined jsProps then []
-        else
-            objectKeys(jsProps)
-            |> Array.map (fun key -> 
-                prop.custom(key, jsProps?(key)))
-            |> Array.toList
-
-    // Merge all props together
-    let mergedProps = 
-        baseProps
-        @ (convertJsProps (dnd?attributes))
-        @ (convertJsProps (dnd?listeners))
-
-    Html.div mergedProps
-
 [<ReactComponent>]
 let Droppable (props: DroppableProps) =
-    let dnd = useDroppableHook {| id = props.id |}
+    let dnd = useDroppable {| id = props.id |}
     
     Html.div [
         prop.ref (unbox dnd?setNodeRef)
@@ -82,25 +73,19 @@ let Droppable (props: DroppableProps) =
             style.padding 10
             style.minHeight 60
             style.marginTop 10
-            style.display.flex
-            style.alignItems.center
-            style.justifyContent.center
             style.backgroundColor (if unbox<bool> dnd?isOver then "#dbeafe" else "#f9fafb")
         ]
     ]
 
 [<ReactComponent>]
 let DndContext (onDragEnd: obj -> unit) (children: ReactElement list) =
-    let sensors = 
-        useSensorsHook(
-            useSensorHook(pointerSensor)
-        )
+    let sensors = useSensors(useSensor(pointerSensor))
     
     ReactBindings.React.createElement(
-        dndContextComponent,
+        dndContext,
         createObj [
             "onDragEnd" ==> onDragEnd
             "sensors" ==> sensors
         ],
-        children
+        List.toArray children
     )
